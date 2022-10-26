a2160021fd23eef35e6a9ea1667a597be343eac4
Zach L <ZachLaba4@gmail.com>
2022-10-26 13:36:08 -0400

    Update
    Added C++

---
+++ Source/FortniteGameMain/Public/ComLockTest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ClassTemplateEditorSubsystem.h"
+#include "ComLockTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FORTNITEGAMEMAIN_API UComLockTest : public UClassTemplate
+{
+	GENERATED_BODY()
+	
+};
